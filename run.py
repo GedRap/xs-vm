8ca9d3fd0866d6db87a243d9e1613f53d264dc40
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2015-06-26 22:10:27 +0300

    dump register values after executing

--- run.py
+++ run.py
@@ -5,3 +5,6 @@ filename = "demos/function_call.s"
 parser.load_file_into_memory(machine.memory, filename)
 
 machine.execute_until_halted()
+
+# dump all register values. will do the job for now :)
+print machine.register_bank.registers
