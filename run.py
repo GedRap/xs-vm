a106f62a5a4d2b8aa3049e81ffb5f9315e2ba772
Chris Seymour <chris.j.seymour@hotmail.com>
2015-08-11 20:21:59 +0100

    made the instructions limit available via the cli

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 args = ArgumentParser()
 args.add_argument("filename")
 args.add_argument("-d", "--debug", action="store_true", default=False)
+args.add_argument("-m", "--max-instructions", metavar='', type=int, default=5000)
 args = args.parse_args()
 
 print "Debug mode: {d}".format(d=args.debug)
@@ -12,7 +13,7 @@ print "Debug mode: {d}".format(d=args.debug)
 machine = vm.Processor(debug=args.debug)
 parser.load_file_into_memory(machine.memory, args.filename)
 
-machine.execute_until_halted(instructions_limit=5000)
+machine.execute_until_halted(instructions_limit=args.max_instructions)
 
 print "Instructions executed: {i}".format(i=machine.instructions_executed)
 print "Instructions executed by type:"
